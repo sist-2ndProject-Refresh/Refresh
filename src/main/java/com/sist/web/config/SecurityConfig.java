88b4db7b259cb426d6666d8b5201579f8a991bf1
sunghyun9737 <sunghyun9737@gmail.com>
2026-01-08 17:51:24 +0900

    security start

---
+++ src/main/java/com/sist/web/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.sist.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+
+public class SecurityConfig {
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/").permitAll()
+				.anyRequest().permitAll()
+			)
+			.formLogin(form -> form
+				.loginPage("/member/login")
+				.loginProcessingUrl("/member/login_ok")
+				.defaultSuccessUrl("/",true)
+			)
+			.logout(logout -> logout
+				.logoutSuccessUrl("/")
+			);
+		
+		
+		
+		return http.build();
+	}
+}
